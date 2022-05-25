53cf68769f5b40d776bec509b07150dedc910148
emreblbl <emrevlog0@gmail.com>
2022-05-25 22:28:58 +0300

    new messages added to Constant.java

--- src/main/java/com/example/flight/base/util/Constant.java
+++ src/main/java/com/example/flight/base/util/Constant.java
@@ -8,6 +8,9 @@ public class Constant {
     public static final int FAIL_RESULT_CODE=0;
     public static final String AIRLINE_MANDATORY_MESSAGE = "IATA Code must be 2 characters";
     public static final String AIRPORT_MANDATORY_MESSAGE ="IATA Code must be 3 characters";
+    // Normally we can get airport and airline name from class.getName() with dynamicly and then
+    // concatenate with static message to send class validation annotation.
     public static final String AIRPORT_NAME_MANDATORY_MESSAGE = "Airport name is mandotary";
+    public static final String AIRLINE_NAME_MANDATORY_MESSAGE = "airline name is mandotary";
 }
 
