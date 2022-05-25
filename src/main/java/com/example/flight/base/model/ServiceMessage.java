7c97c55ec9e1cdf6ddb8025be78668c589aa7c8c
emreblbl <emrevlog0@gmail.com>
2022-05-25 20:58:59 +0300

    ServiceMessage enum created for getting constant HTTP body's message code.

---
+++ src/main/java/com/example/flight/base/model/ServiceMessage.java
@@ -0,0 +1,12 @@
+package com.example.flight.base.model;
+
+public enum ServiceMessage {
+    INSERT_SUCCESS(),
+    INSERT_FAILED(),
+    GET_SUCCESS(),
+    GET_FAILED(),
+    UPDATE_SUCCESS(),
+    UPDATE_FAILED(),
+    DELETE_SUCCESS(),
+    DELETE_FAILED()
+}
